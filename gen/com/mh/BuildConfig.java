4f4d9fa30850a42051a091dfe5248f4d5ecd3b83
hanxuan.mh <hanxuan.mh@taobao.com>
2013-10-09 19:31:05 +0800

    fix error

---
+++ gen/com/mh/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.mh;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- gen/com/mh/R.java
+++ gen/com/mh/R.java
@@ -29,52 +29,33 @@ public final class R {
         public static final int widget_4x1_bkg=0x7f02000f;
     }
     public static final class id {
-        public static final int HotAreaDownLayout=0x7f06001c;
-        public static final int HourLeftNumber=0x7f060010;
-        public static final int HourRightNumber=0x7f060011;
-        public static final int ItemText=0x7f060002;
-        public static final int ItemTitle=0x7f060001;
-        public static final int MinuteLeftNumber=0x7f060012;
-        public static final int MinuteRightNumber=0x7f060013;
-        public static final int RelativeLayout01=0x7f060000;
-        public static final int TextInfoLeftHotArea=0x7f06001d;
-        public static final int TextInfoRightHotArea=0x7f06001e;
-        public static final int bar_title=0x7f06000f;
-        public static final int classList=0x7f06000e;
-        public static final int discardButton=0x7f06000c;
-        public static final int head_gallery=0x7f06000d;
-        public static final int infoET=0x7f06000a;
-        public static final int placeET=0x7f060007;
-        public static final int placeTV1=0x7f060018;
-        public static final int placeTV2=0x7f06001b;
-        public static final int saveButton=0x7f06000b;
-        public static final int sequenceTV=0x7f060005;
-        public static final int sequenceTV1=0x7f060016;
-        public static final int sequenceTV2=0x7f060019;
-        public static final int subjectET=0x7f060006;
-        public static final int subjectTV1=0x7f060017;
-        public static final int subjectTV2=0x7f06001a;
-        public static final int teacherET=0x7f060008;
-        public static final int timeET=0x7f060009;
-        public static final int weekNameTV=0x7f060004;
-        public static final int widget40=0x7f060003;
-        public static final int widgetDateTV=0x7f060014;
-        public static final int widgetWeekTV=0x7f060015;
+        public static final int ItemText=0x7f050002;
+        public static final int ItemTitle=0x7f050001;
+        public static final int RelativeLayout01=0x7f050000;
+        public static final int bar_title=0x7f05000f;
+        public static final int classList=0x7f05000e;
+        public static final int discardButton=0x7f05000c;
+        public static final int head_gallery=0x7f05000d;
+        public static final int infoET=0x7f05000a;
+        public static final int placeET=0x7f050007;
+        public static final int saveButton=0x7f05000b;
+        public static final int sequenceTV=0x7f050005;
+        public static final int subjectET=0x7f050006;
+        public static final int teacherET=0x7f050008;
+        public static final int timeET=0x7f050009;
+        public static final int weekNameTV=0x7f050004;
+        public static final int widget40=0x7f050003;
     }
     public static final class layout {
         public static final int class_list=0x7f030000;
         public static final int eidt_class=0x7f030001;
         public static final int main=0x7f030002;
         public static final int toolbar_row=0x7f030003;
-        public static final int widget_4x1=0x7f030004;
     }
     public static final class string {
-        public static final int app_name=0x7f050001;
-        public static final int hello=0x7f050000;
-        public static final int menu_doNotSave=0x7f050003;
-        public static final int menu_done=0x7f050002;
-    }
-    public static final class xml {
-        public static final int widget_info=0x7f040000;
+        public static final int app_name=0x7f040001;
+        public static final int hello=0x7f040000;
+        public static final int menu_doNotSave=0x7f040003;
+        public static final int menu_done=0x7f040002;
     }
 }
