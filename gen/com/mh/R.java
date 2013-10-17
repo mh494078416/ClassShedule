65e83b25ae59b1c45c565c3e1fbd63379aad7e15
hanxuan.mh <hanxuan.mh@taobao.com>
2013-10-17 22:57:19 +0800

    smart bar

--- gen/com/mh/R.java
+++ gen/com/mh/R.java
@@ -11,27 +11,28 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_new=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int ItemText=0x7f060002;
-        public static final int ItemTitle=0x7f060001;
-        public static final int RelativeLayout01=0x7f060000;
-        public static final int bar_title=0x7f060010;
-        public static final int classList=0x7f06000d;
-        public static final int discardButton=0x7f06000b;
-        public static final int infoET=0x7f06000a;
-        public static final int pager=0x7f06000e;
-        public static final int pager_title_strip=0x7f06000f;
-        public static final int placeET=0x7f060007;
-        public static final int saveButton=0x7f06000c;
-        public static final int sequenceTV=0x7f060005;
-        public static final int subjectET=0x7f060006;
-        public static final int teacherET=0x7f060008;
-        public static final int timeET=0x7f060009;
-        public static final int weekNameTV=0x7f060004;
-        public static final int widget40=0x7f060003;
+        public static final int ItemText=0x7f070002;
+        public static final int ItemTitle=0x7f070001;
+        public static final int RelativeLayout01=0x7f070000;
+        public static final int action_share=0x7f070012;
+        public static final int bar_title=0x7f070010;
+        public static final int classList=0x7f07000d;
+        public static final int discardButton=0x7f07000b;
+        public static final int infoET=0x7f07000a;
+        public static final int menu_new=0x7f070011;
+        public static final int pager=0x7f07000e;
+        public static final int pager_title_strip=0x7f07000f;
+        public static final int placeET=0x7f070007;
+        public static final int saveButton=0x7f07000c;
+        public static final int sequenceTV=0x7f070005;
+        public static final int subjectET=0x7f070006;
+        public static final int teacherET=0x7f070008;
+        public static final int timeET=0x7f070009;
+        public static final int weekNameTV=0x7f070004;
+        public static final int widget40=0x7f070003;
     }
     public static final class layout {
         public static final int class_list=0x7f030000;
@@ -40,13 +41,15 @@ public final class R {
         public static final int main=0x7f030003;
         public static final int toolbar_row=0x7f030004;
     }
+    public static final class menu {
+        public static final int action_bar_list_menu=0x7f060000;
+    }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int menu_doNotSave=0x7f040002;
         public static final int menu_done=0x7f040001;
     }
     public static final class style {
-        public static final int AppBaseTheme=0x7f050000;
-        public static final int AppTheme=0x7f050001;
+        public static final int Theme_Holo_Light_SmartBar=0x7f050000;
     }
 }
--- src/com/mh/activity/MainView.java
+++ src/com/mh/activity/MainView.java
@@ -1,11 +1,17 @@
 package com.mh.activity;
 
+import android.app.ActionBar;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 import com.mh.R;
 import com.mh.util.MyConstant;
 
@@ -19,7 +25,10 @@ public class MainView extends FragmentActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-
+		final ActionBar bar = getActionBar();
+		ColorDrawable colorDrawable = new ColorDrawable();
+		colorDrawable.setColor(0xff000000);
+		bar.setSplitBackgroundDrawable(colorDrawable);
 		shedulePagerAdapter = new ShedulePagerAdapter(
 				getSupportFragmentManager());
 
@@ -27,8 +36,25 @@ public class MainView extends FragmentActivity {
 		mViewPager.setAdapter(shedulePagerAdapter);
 	}
 
-	public static class ShedulePagerAdapter extends
-			FragmentStatePagerAdapter {
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.action_bar_list_menu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_new:
+			break;
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public static class ShedulePagerAdapter extends FragmentStatePagerAdapter {
 
 		public ShedulePagerAdapter(FragmentManager fm) {
 			super(fm);
